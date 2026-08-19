fff951adc71245653cfb920744881d34634fc554
David Ruiz de Olano Apodaca <druiz@deusto.es>
2026-08-19 11:20:09 +0200

    Restaura la URL real del servidor en config.js

--- assets/config.js
+++ assets/config.js
@@ -8,4 +8,4 @@
  *
  * Es lo único que hay que tocar de la web una vez desplegado el servidor.
  */
-window.NEGOCIADOR_API = 'https://PON-AQUI-TU-SERVICIO.onrender.com';
+window.NEGOCIADOR_API = 'https://negociador-tabs.onrender.com';
